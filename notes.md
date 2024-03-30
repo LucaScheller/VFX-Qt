2e56b5dd49e86ca65726da612a207e8a9a6b64fe
Luca Scheller <luca.scheller@gmail.com>
2024-03-30 15:21:53 -0700

    Add modules

---
+++ notes.md
@@ -0,0 +1,13 @@
+# Repo Setup
+> export REPO_ROOT=$PROJECT/VFX-Qt
+## Sub-Modules
+> git submodule add https://github.com/LucaScheller/NodeGraphQt.git external/NodeGraphQt
+## Python
+> python3.9 -m venv $REPO_ROOT/external/python
+Manually edit shell line prefix to "VFX-Qt-Python" in:
+$REPO_ROOT/external/python/bin/activate
+### Dependency Links
+> ln -s -r external/NodeGraphQt/NodeGraphQt external/python/lib/python3.9/site-packages/NodeGraphQt
+### Pip Installs
+> source $REPO_ROOT/external/python/bin/activate
+> pip install -r requirements.txt 
--- setup.sh
+++ setup.sh
@@ -3,6 +3,11 @@ if [ ! $REPO_SOURCED ]
 then
     export REPO_SOURCED=1
     export REPO_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && (pwd -W 2> /dev/null || pwd))
+    # Source Python
+    source $REPO_ROOT/external/python/bin/activate
+    export PYTHONDONTWRITEBYTECODE=1 # Disable __pycache__ byte code generation
+    # Add external Python packages
+    export PYTHONPATH=$REPO_ROOT/src:$DEADLINE_INSTALL_ROOT/DeadlineRepository10/api/python:$DEADLINE_INSTALL_ROOT/DeadlineRepository10/custom/library/python:$PYTHONPATH
     # Source Houdini
     pushd /opt/hfs20.0 > /dev/null
     source houdini_setup
